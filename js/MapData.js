96435cd3d5940cc3dd14e2cbfd45a4f1fa60213f
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-04-25 21:31:24 -0400

    Set up path and projection vars

--- js/MapData.js
+++ js/MapData.js
@@ -10,9 +10,17 @@ d3.json(dataUrl, function(json) {
 
 	d3.select(".mapdata-title").text("Map of Global Meteorite Landings");
 
+	var projection = d3.geoMercator()
+		.scale(100)
+		.translate([width/2, height/2]);
+
 	var svg = d3.select(".mapdata")
 		.append("svg")
 		.attr("width", width)
 		.attr("height", height)
-		.attr("viewBox", "0 0 600 600");
+		.append("g")
+		.attr("class", "map");
+
+	var path = d3.geoPath().projection(projection);
+
 })
